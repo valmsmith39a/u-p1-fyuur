50c28977e198f12346c89d62a33e1623cc443158
valmsmith39a <valmsmith39a@gmail.com>
2021-06-15 20:11:30 -0700

    get all venues

--- projects/01_fyyur/starter_code/app_notes.py
+++ projects/01_fyyur/starter_code/app_notes.py
@@ -24,3 +24,31 @@ def venues():
     }]
   }]
   return render_template('pages/venues.html', areas=data);
+
+@app.route('/venues')
+def venues():
+  # TODO: replace with real venues data.
+  #       num_shows should be aggregated based on number of upcoming shows per venue.
+  data=[{
+    "city": "San Francisco",
+    "state": "CA",
+    "venues": [{
+      "id": 1,
+      "name": "The Musical Hop",
+      "num_upcoming_shows": 0,
+    }, {
+      "id": 3,
+      "name": "Park Square Live Music & Coffee",
+      "num_upcoming_shows": 1,
+    }]
+  }, {
+    "city": "New York",
+    "state": "NY",
+    "venues": [{
+      "id": 2,
+      "name": "The Dueling Pianos Bar",
+      "num_upcoming_shows": 0,
+    }]
+  }]
+  return render_template('pages/venues.html', areas=data);
+
